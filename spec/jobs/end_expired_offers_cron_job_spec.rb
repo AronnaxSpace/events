e99eb74c42f49bb86ba02e729854c8525ac89137
ihortok <itokaryk@gmail.com>
2023-11-30 18:30:52 +0100

    offer time formats

--- spec/jobs/end_expired_offers_cron_job_spec.rb
+++ spec/jobs/end_expired_offers_cron_job_spec.rb
@@ -2,7 +2,7 @@ describe EndExpiredOffersCronJob do
   subject(:job) { described_class.new.perform }
 
   before do
-    offer = create(:offer, :published, start_at: 2.days.ago)
+    offer = build(:offer, :published, start_at: 2.days.ago)
     offer.end_at = 1.day.ago
 
     offer.save(validate: false)
