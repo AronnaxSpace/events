694a02ad7e08bfa59b8c84fc8d8506c982a4acb9
ihortok <itokaryk@gmail.com>
2025-01-16 18:21:37 +0100

    rails app:update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_23_190743) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_16_171634) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
